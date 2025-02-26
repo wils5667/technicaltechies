1d00477e4e6aca6aec3ddb97fa18a800e3830b1c
JavierAAS <157152701+JaviWasHere7@users.noreply.github.com>
2025-02-26 16:52:10 -0700

    Added more comments

--- flask/app.py
+++ flask/app.py
@@ -10,3 +10,8 @@ def initialize_alcohol_items():
             {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120},
             {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120}
         ]
+"""
+@app.route('/')
+def base():
+    return render_template('base.html')
+"""
