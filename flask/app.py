22118888580682d117746be559a8fc8550df2bbe
JavierAAS <157152701+JaviWasHere7@users.noreply.github.com>
2025-02-26 16:11:45 -0700

    Added the base for website

---
+++ flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, url_for, session, request
+
+app = Flask(__name__)
+
+@app.before_request
+def initialize_alcohol_items():
+    if 'alcohol_items' not in session:
+        session['alcohol_items'] = [
+            {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120},
+            {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120},
+            {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120}
+        ]
