42c4e6b37f3a54f8bfa14ae56beedf4dbc43050d
JavierAAS <157152701+JaviWasHere7@users.noreply.github.com>
2025-02-27 10:13:39 -0700

    Removed Venv

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 
 app = Flask(__name__)
-
+"""
 @app.before_request
 def initialize_alcohol_items():
     if 'alcohol_items' not in session:
@@ -10,6 +10,13 @@ def initialize_alcohol_items():
             {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120},
             {"name": "Jack Daniel's Old No. 7", "serial": "JD001", "expiration": "2025-12-15", "quantity": 120}
         ]
+"""
+        
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World</p>"
+
+
 """
 @app.route('/')
 def base():
