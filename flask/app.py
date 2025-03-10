cbb0ab7a4716db7a38653357086994bea889e1ed
JavierAAS <157152701+JaviWasHere7@users.noreply.github.com>
2025-03-10 16:54:27 -0600

    Connect to MongoDB
    Need to add more features

--- flask/app.py
+++ flask/app.py
@@ -1,30 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, session, request, jsonify
-from flask_pymongo import PyMongo
+from flask import Flask
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://JavierAAS46:Lostmy-63313113@techiescluster.0zyk4.mongodb.net/?retryWrites=true&w=majority&appName=TechiesCluster"
+uri = "mongodb+srv://javierarroyosolis46:Lostmy-63313113@techiescluster.0zyk4.mongodb.net/?retryWrites=true&w=majority&appName=TechiesCluster"
 
-mongo = PyMongo(app)
+client = MongoClient(uri)
+db = client["techies_dbs"]
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "MongoDB connected successfully"
-
-if __name__ == "__main__":
+    try:
+        client.admin.command('ping')
+        return "Pinged your deployment. You successfully connected to MongoDB!"
+    except Exception as e:
+        return e
+    
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-@app.route("/products", methods=['GET'])
-def get_products():
-    products = mongo.db.products.find()
-    product_list = [{
-        "id": str(product["_id"]),
-        "name": product["productName"],
-        "brand": product["brand"],
-        "catagory": product["catagory"],
-        "productID": product["productID"],
-        "volume": product["volume"]
-    } for product in products]
-
-    return jsonify(product_list)
